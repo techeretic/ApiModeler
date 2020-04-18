be371e1730d4b663ecf94826cf05d5ec52dbff22
Prathamesh Shetye <pshetye@pinterest.com>
2020-04-18 12:02:53 -0700

    Added a placeholder search modal fragment

---
+++ covid19/src/main/java/com/pshetye/covid19/ui/countries/fragments/SearchDialogFragment.kt
@@ -0,0 +1,71 @@
+package com.pshetye.covid19.ui.countries.fragments
+
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.pshetye.covid19.R
+import kotlinx.android.synthetic.main.fragment_search_dialog_list_dialog.*
+import kotlinx.android.synthetic.main.fragment_search_dialog_list_dialog_item.view.*
+
+// TODO: Customize parameter argument names
+const val ARG_ITEM_COUNT = "item_count"
+
+/**
+ *
+ * A fragment that shows a list of items as a modal bottom sheet.
+ *
+ * You can show this modal bottom sheet from your activity like this:
+ * <pre>
+ *    SearchDialogFragment.newInstance(30).show(supportFragmentManager, "dialog")
+ * </pre>
+ */
+class SearchDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_dialog_list_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        list.layoutManager = LinearLayoutManager(context)
+        list.adapter = StringAdapter(0)
+    }
+
+    private inner class ViewHolder internal constructor(
+        inflater: LayoutInflater,
+        parent: ViewGroup
+    ) : RecyclerView.ViewHolder(
+        inflater.inflate(
+            R.layout.fragment_search_dialog_list_dialog_item,
+            parent,
+            false
+        )
+    ) {
+
+        internal val text: TextView = itemView.text
+    }
+
+    private inner class StringAdapter internal constructor(private val mItemCount: Int) :
+        RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(parent.context), parent)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.text.text = position.toString()
+        }
+
+        override fun getItemCount(): Int {
+            return mItemCount
+        }
+    }
+
+}
